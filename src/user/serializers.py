562e7c4474652274978be905bd670a5843ced01d
Mateus Siqueira <mateusiqueira.s@gmail.com>
2022-08-28 21:48:05 -0300

    Adicionando Swagger e parte de Autenticacao

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -1,10 +1,95 @@
 from django.contrib.auth import authenticate
-
-from rest_framework import serializers
+from .models import User
 from user import models
+from rest_framework import serializers
+from .models import User
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 
-class LoginSerializer(serializers.Serializer):
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+
+    default_error_messages = {
+        'username': 'The username should only contain alphanumeric characters'}
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+
+        if not username.isalnum():
+            raise serializers.ValidationError(
+                self.default_error_messages)
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = User
+        fields = ['token']
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+    username = serializers.CharField(
+        max_length=255, min_length=3, read_only=True)
+
+    tokens = serializers.SerializerMethodField()
+
+    def get_tokens(self, obj):
+        user = User.objects.get(email=obj['email'])
+
+        return {
+            'refresh': user.tokens()['refresh'],
+            'access': user.tokens()['access']
+        }
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'tokens']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        filtered_user_by_email = User.objects.filter(email=email)
+        user = auth.authenticate(email=email, password=password)
+
+        if filtered_user_by_email.exists() and filtered_user_by_email[0].auth_provider != 'email':
+            raise AuthenticationFailed(
+                detail='Please continue your login using ' + filtered_user_by_email[0].auth_provider)
+
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+        if not user.is_verified:
+            raise AuthenticationFailed('Email is not verified')
+
+        return {
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens
+        }
+
+        return super().validate(attrs)
+
+# class LoginSerializer(serializers.Serializer):
     """
     This serializer defines two fields for authentication:
       * username
@@ -30,7 +115,8 @@ class LoginSerializer(serializers.Serializer):
 
         if username and password:
             # Try to authenticate the user using Django auth framework.
-            user = authenticate(request=self.context.get('request'), username=username, password=password)
+            user = authenticate(request=self.context.get(
+                'request'), username=username, password=password)
             if not user:
                 # If we don't have a regular user, raise a ValidationError
                 msg = 'Access denied: wrong username or password.'
@@ -46,12 +132,12 @@ class LoginSerializer(serializers.Serializer):
 
 class UserSerializer(serializers.ModelSerializer):
 
-   class Meta:
-       model = models.User
-       fields = [
-           'username',
-           'email',
-           'first_name',
-           'last_name',
-           'date_joined',
-       ]
+    class Meta:
+        model = models.User
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'date_joined',
+        ]
--- src/user/viewsets.py
+++ src/user/viewsets.py
@@ -1,5 +1,6 @@
 # from rest_framework import viewsets
-# from user import serializers
+from lib2to3.pgen2 import token
+from user import serializers
 # from user import models
 
 
@@ -7,34 +8,88 @@
 #     serializer_class = serializers.UserSerializer
 #     queryset = models.User.objects.all()
 
-from django.contrib.auth import login, logout, get_user_model
-
-from rest_framework import generics
-from rest_framework import permissions
-from rest_framework import status
-from rest_framework import views
+from django.shortcuts import render
+from rest_framework import generics, status, views, permissions
+from .serializers import EmailVerificationSerializer, RegisterSerializer, LoginSerializer
+# , SetNewPasswordSerializer, ResetPasswordEmailRequestSerializer, EmailVerificationSerializer, LogoutSerializer
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+import jwt
+from django.conf import settings
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+# from .renderers import UserRenderer
+# from django.contrib.auth.tokens import PasswordResetTokenGenerator
+# from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+# from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from .utils import Util
+# from django.shortcuts import redirect
+# from django.http import HttpResponsePermanentRedirect
+# import os
 
-from user import serializers
 
+class RegisterView(generics.GenericAPIView):
 
-class LoginView(views.APIView):
-    # This view should be accessible also for unauthenticated users.
-    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+    # renderer_classes = (UserRenderer,)
 
-    def post(self, request, format=None):
-        serializer = serializers.LoginSerializer(data=request.data, context={ 'request': request })
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return Response(None, status=status.HTTP_202_ACCEPTED)
+        serializer.save()
+        user_data = serializer.data
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = 'Hi '+user.username + \
+            ' Use the link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
+
+        print(data)
+
+        # Util.send_email(data)
+        return Response(user_data, status=status.HTTP_201_CREATED)
 
 
-class LogoutView(views.APIView):
+class VerifyEmail(views.APIView):
+    serializer_class = RegisterSerializer
 
-    def post(self, request, format=None):
-        logout(request)
-        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    token_param_config = openapi.Parameter(
+        'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 # classe para pegar os dados do usuario, requer que o usuario esteja autenticado
@@ -43,13 +98,3 @@ class ProfileView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
-
-
-# TODO: averiguar (e talvez aumentar) a segurança desta view
-class RegisterView(views.APIView):
-    # This view should be accessible also for unauthenticated users.
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, format=None):
-        get_user_model().objects.create_user(**request.data)
-        return Response(None, status=status.HTTP_202_ACCEPTED)
