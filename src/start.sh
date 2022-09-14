b67134a165ab22566777cafa6f52123ff99a28b1
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2022-09-13 22:03:05 -0300

    consertar ambiente de debug e remover username

--- src/start.sh
+++ src/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sleep 30
 python manage.py flush --no-input
 python manage.py migrate
-./manage.py createsuperuser --no-input --username=${DJANGO_SUPERUSER_USERNAME} --email=leonardomichalskim@gmail.com
+./manage.py createsuperuser --no-input --email=${DJANGO_SUPERUSER_EMAIL}
 gunicorn core.wsgi:application --bind :8080 --workers 3
